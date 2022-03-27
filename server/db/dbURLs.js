694cb2c526b75c8c782528b51c9af922175bdd59
mehdi parastar <parastar.mehdi@gmail.com>
2022-03-28 01:12:11 +0430

    first commit

--- server/db/dbURLs.js
+++ server/db/dbURLs.js
@@ -1,6 +1,6 @@
 const dbUrls = {
-    baseUrl: `${process.env.REPLICA_URL}/base?replicaSet=${process.env.REPLICA_NAME}`,
-    userUrl: `${process.env.REPLICA_URL}/user?replicaSet=${process.env.REPLICA_NAME}`,
+    baseUrl: process.env.REPLICA_URL ? `${process.env.REPLICA_URL}/base?replicaSet=${process.env.REPLICA_NAME}` : "mongodb://127.0.0.1:27021,127.0.0.1:27022,127.0.0.1:27023/base?replicaSet=dbrs",
+    userUrl: process.env.REPLICA_URL ? `${process.env.REPLICA_URL}/user?replicaSet=${process.env.REPLICA_NAME}` : "mongodb://127.0.0.1:27021,127.0.0.1:27022,127.0.0.1:27023/user?replicaSet=dbrs",
 }
 
 console.log(dbUrls)
